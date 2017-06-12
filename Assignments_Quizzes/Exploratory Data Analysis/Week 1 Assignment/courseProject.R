1710ababec9aa2ac82ec1198b262ea4144851e88
Grejell Segura <mr_grej@yahoo.com>
2017-06-12 11:39:01 +0400

    Update courseProject.R

--- Assignments_Quizzes/Exploratory Data Analysis/Week 1 Assignment/courseProject.R
+++ Assignments_Quizzes/Exploratory Data Analysis/Week 1 Assignment/courseProject.R
@@ -1,3 +1,4 @@
+## 1.
 
 library(data.table)
 library(lubridate)
@@ -5,27 +6,89 @@ library(lubridate)
 unzip("/Users/Grejell/Desktop/R/Coursera/Course 4 - Exploratory DA/Week 1 - Course Proj/exdata%2Fdata%2Fhousehold_power_consumption.zip")
 
 hpc_data <- fread("/Users/Grejell/Desktop/R/Coursera/Course 4 - Exploratory DA/Week 1 - Course Proj/household_power_consumption.txt", 
-                  header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".", na.strings = "?")
-## hpc_data <- read.table("/Users/Grejell/Desktop/R/Coursera/Course 4 - Exploratory DA/Week 1 - Course Proj/household_power_consumption.txt", 
-##                       header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-
+                  header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".", na.strings = "?")
 
 str(hpc_data)
 hpc_data <- hpc_data[, Date := dmy(Date)]
 hpc_data_new <- hpc_data[Date == "2007-02-01" | Date == "2007-02-02", ]
 any(is.na(hpc_data_new))
 
-## 1.
-globalActivePower <- hpc_data_new$Global_active_power
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+png("./plot1.png", width = 480, height = 480)
+hist(hpc_data_new$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
+
 ## 2.
+
+library(data.table)
+library(lubridate)
+
+unzip("/Users/Grejell/Desktop/R/Coursera/Course 4 - Exploratory DA/Week 1 - Course Proj/exdata%2Fdata%2Fhousehold_power_consumption.zip")
+
+hpc_data <- fread("/Users/Grejell/Desktop/R/Coursera/Course 4 - Exploratory DA/Week 1 - Course Proj/household_power_consumption.txt", 
+                  header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".", na.strings = "?")
+
+str(hpc_data)
+hpc_data <- hpc_data[, Date := dmy(Date)]
 hpc_data_new <- hpc_data[Date == "2007-02-01" | Date == "2007-02-02", ]
-hpc_data_new <- hpc_data_new[, Time := as.POSIXct(Time)]
 any(is.na(hpc_data_new))
 
-datetime <- strptime(paste(hpc_data_new$Date, hpc_data_new$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(hpc_data_new$Global_active_power)
-png("plot2.png", width=480, height=480)
-plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+hpc_data_new <- hpc_data_new[, Date_Time := ymd_hms(paste(Date, Time))]
+png("./plot2.png", width = 480, height = 480)
+plot(hpc_data_new$Date_Time, hpc_data_new$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
+
+## 3. 
+library(data.table)
+library(lubridate)
+
+unzip("/Users/Grejell/Desktop/R/Coursera/Course 4 - Exploratory DA/Week 1 - Course Proj/exdata%2Fdata%2Fhousehold_power_consumption.zip")
+
+hpc_data <- fread("/Users/Grejell/Desktop/R/Coursera/Course 4 - Exploratory DA/Week 1 - Course Proj/household_power_consumption.txt", 
+                  header = TRUE, sep = ";", stringsAsFactors=FALSE, dec = ".", na.strings = "?")
+
+str(hpc_data)
+hpc_data <- hpc_data[, Date := dmy(Date)]
+hpc_data_new <- hpc_data[Date == "2007-02-01" | Date == "2007-02-02", ]
+any(is.na(hpc_data_new))
+
+hpc_data_new <- hpc_data_new[, Date_Time := ymd_hms(paste(Date, Time))]
+
+png("./plot3.png", width = 480, height = 480)
+plot(hpc_data_new$Date_Time, hpc_data_new$Sub_metering_1, type = "l", xlab = "", ylab = "Energy Submetering")
+lines(hpc_data_new$Date_Time, hpc_data_new$Sub_metering_2, type = "l", xlab = "", ylab = "", col = "red")
+lines(hpc_data_new$Date_Time, hpc_data_new$Sub_metering_3, type = "l", xlab = "", ylab = "", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5, col = c("black", "red", "blue"))
+dev.off()
+
+
+
+## 4. 
+library(data.table)
+library(lubridate)
+
+unzip("/Users/Grejell/Desktop/R/Coursera/Course 4 - Exploratory DA/Week 1 - Course Proj/exdata%2Fdata%2Fhousehold_power_consumption.zip")
+
+hpc_data <- fread("/Users/Grejell/Desktop/R/Coursera/Course 4 - Exploratory DA/Week 1 - Course Proj/household_power_consumption.txt", 
+                  header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".", na.strings = "?")
+
+str(hpc_data)
+hpc_data <- hpc_data[, Date := dmy(Date)]
+hpc_data_new <- hpc_data[Date == "2007-02-01" | Date == "2007-02-02", ]
+any(is.na(hpc_data_new))
+
+hpc_data_new <- hpc_data_new[, Date_Time := ymd_hms(paste(Date, Time))]
+
+png("./plot4.png", width = 480, height = 480)
+par(mfrow = c(2, 2)) 
+plot(hpc_data_new$Date_Time, hpc_data_new$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+plot(hpc_data_new$Date_Time, hpc_data_new$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+
+plot(hpc_data_new$Date_Time, hpc_data_new$Sub_metering_1, type = "l", xlab = "", ylab = "Energy Submetering")
+lines(hpc_data_new$Date_Time, hpc_data_new$Sub_metering_2, type = "l", xlab = "", ylab = "", col = "red")
+lines(hpc_data_new$Date_Time, hpc_data_new$Sub_metering_3, type = "l", xlab = "", ylab = "", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5, col = c("black", "red", "blue"))
+
+plot(hpc_data_new$Date_Time, hpc_data_new$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+
+dev.off()
